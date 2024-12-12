9cf64876a0f22f893ecba4d3f8cd4000cc60d909
AirStonks <changyongkang1001@gmail.com>
2024-12-12 15:23:40 +0800

    change add migration

--- database/migrations/2024_12_08_020210_add_table_column.php
+++ database/migrations/2024_12_08_020210_add_table_column.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::table('products', function (Blueprint $table) {
-        //     $table->json('attachments')->nullable()->after('status');
-        // });
+        Schema::table('products', function (Blueprint $table) {
+            $table->json('attachments')->nullable()->after('status');
+        });
     }
 
     /**
