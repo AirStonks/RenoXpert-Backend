ab704880d9cb02f23a6e24bcbfd59c238d4fb122
AirStonks <changyongkang1001@gmail.com>
2025-01-20 10:45:04 +0800

    Test file upload

--- app/Http/Controllers/DefectInspectionFormController.php
+++ app/Http/Controllers/DefectInspectionFormController.php
@@ -245,7 +245,7 @@ class DefectInspectionFormController extends BaseController
                     foreach ($questions as $q => $question) {
                         if ($request->input('question') === $q) {
 
-                                return $this->sendError($request->hasFile('attachment'));
+                                return $this->sendError($request->file('attachment'));
                             if ($request->input('value')) {
                                 $question->value = $request->input('value');
                             } else if ($request->input('remark')) {
