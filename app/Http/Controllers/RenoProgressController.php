8eccdd5dad6abc34c7e4e819f4e4f70e0347d906
AirStonks <changyongkang1001@gmail.com>
2025-05-27 11:26:10 +0800

    testing

--- app/Http/Controllers/RenoProgressController.php
+++ app/Http/Controllers/RenoProgressController.php
@@ -367,6 +367,8 @@ class RenoProgressController extends BaseController
 
         $renoProgress = RenoProgress::find($id);
 
+        return $this->sendError($renoProgress->sale->user->id, null, 403);
+
         // Check if the reno progress is retrieve by the current user
         if (is_null($renoProgress) || $renoProgress->sale->user->id != $user->id) {
             return $this->sendError('Invalid Credential.', null, 403);
