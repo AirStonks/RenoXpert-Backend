ee8f68f5fac2671246508b231258dacbd03898a9
AirStonks <changyongkang1001@gmail.com>
2025-07-23 16:35:33 +0800

    minor fix

--- database/migrations/2024_12_08_020210_add_table_column.php
+++ database/migrations/2024_12_08_020210_add_table_column.php
@@ -37,11 +37,6 @@ return new class extends Migration
         //     $table->json('investment_goals')->after('rental_strategy');
         //     $table->string('expected_rental_return')->after('rental_strategy');
         // });
-
-        // Staging: Done
-        Schema::table('orders', function (Blueprint $table) {
-            $table->boolean('is_be_powered')->default(false)->after('is_progressive_payment');
-        });
     }
 
     public function down()
